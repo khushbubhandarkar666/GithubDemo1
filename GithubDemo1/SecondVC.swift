fcd7c60fc88b86302ee08b676d9867ed0765e56b
admin <admin@admins-macbook-pro.local>
2018-12-04 14:24:12 +0530

    commit this one only.

---
+++ GithubDemo1/SecondVC.swift
@@ -0,0 +1,27 @@
+//
+//  SecondVC.swift
+//  GithubDemo1
+//
+//  Created by admin on 12/4/18.
+//  Copyright © 2018 Ramanora Global Private Limited. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("push just this vc changes!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+}
